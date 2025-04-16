c5e28491ce53532bbdd24d6865666c097d0aac27
zapsys <zapsyssolutions@gmail.com>
2025-04-15 21:17:28 -0300

    Added function to random key generation

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import random
+import string
+
+
+def key_generator(size=20, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.secret_key = 'hjabggGFGSSHB%%$W99007'
+    app.secret_key = key_generator()
 
     from . import urlshort
+
     app.register_blueprint(urlshort.bp)
 
     return app
